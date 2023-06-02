03dbe4b9e0aec900ab8b7fb3487e58b360ab206d
Derek Truesdale <dtruesdale37@gmail.com>
2023-06-02 17:10:27 -0500

    worked on Top 5

--- index.js
+++ index.js
@@ -1,50 +1,77 @@
 //fetch db.json
+let quotes;
 fetch('http://localhost:3000/quotes')
 .then(res => res.json())
-.then(data => data.forEach(quote => createQuote(quote)))
+.then(data => data.forEach(quote => {
+    quotes= data
+    createQuote(quote)
+}))
+
 
 //Puts all the quotes onto the html
 function createQuote(quote) {
-    const creatediv = document.createElement('li')
-    const quoteContainer = document.querySelector('#quote-container-list')
-    // console.log(quoteContainer)
+    const creatediv = document.createElement('div')
+    const quoteContainer = document.querySelector('#quote-container')
+    creatediv.className = 'quoteList'
+    creatediv.id= quote.id
     creatediv.innerHTML = `
-            ${quote.quote}<br>
-            -${quote.author}<br>
-            <i>${quote.source}</i><br>
+            <quote>${quote.quote}</quote><br>
+            <author>-${quote.author}</author><br>
+            <source><i>${quote.source}</i></source><br>
+            <button class='likeBtn'>Like!</button> <like >Likes: ${quote.like}</like>
+            <br>
             <br>
         `
     quoteContainer.appendChild(creatediv)
+
+    //Like Button
+    document.querySelector('.likeBtn').addEventListener('click',()=>{
+        quote.like++
+        return creatediv.querySelector('like').textContent=`Likes: ${quote.like}`
+    })
 }
 
+
+
 //Highlight a word
-document.querySelector('#quote-container-list').addEventListener('click', (e) => {
+document.querySelector('#quote-container').addEventListener('contextmenu', (e) => {
+    e.preventDefault
+    const clickedElement = e.target
+    // if (clickedElement.nodeName === 'SPAN') {
+    //     console.log('highlighted')
+    //     return clickedElement.classList.toggle('highlight')
+    // }
+    // else{
+    const selectedText = window.getSelection().toString()
+    console.log(selectedText)
+    if (selectedText !== "") {
+        const highlightText = "<span class='highlight'>" + selectedText + "</span>"
+        const newHTML = document.querySelector('#quote-container').innerHTML.replace(selectedText, highlightText)
+        return document.querySelector('#quote-container').innerHTML = newHTML
+    }
+})
+
+//Highlight Off
+document.querySelector('#quote-container').addEventListener('click', (e) => {
     e.preventDefault
     const clickedElement = e.target
     if (clickedElement.nodeName === 'SPAN') {
         console.log('highlighted')
         return clickedElement.classList.toggle('highlight')
     }
-    else{
-        const selectedText = window.getSelection().toString()
-        console.log(selectedText)
-        if(selectedText !==""){
-            const highlightText= "<span class='highlight'>"+selectedText+"</span>"
-            const newHTML = document.querySelector('#quote-container-list').innerHTML.replace(selectedText, highlightText)
-            return document.querySelector('#quote-container-list').innerHTML=newHTML
-        }
-    }
-
 })
-    
 
 
+let text=''
 //Add a quote to the Top 5 List
 document.addEventListener('keydown', (event) => {
+    event.preventDefault()
     const keyChar= event.key
     if (keyChar === '1') {
-        const valueOfOne = mouseOver()
-        console.log(1)
+        text=mouseOver(1)
+        console.log(text)
+        //document.querySelector('#Top1').appendChild(text)
+        return text
     } else if (keyChar === '2') {
         mouseOver()
         console.log(2)
@@ -60,16 +87,17 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-function mouseOver() {
-    document.querySelector('#quote-container-list').addEventListener('mouseover', (e) => {
-        let arr=[e.target.textContent]
-        for(i=0; i<arr.length;i++){
-            const words = arr[i]
-            console.log('Words:', words)
-            return words
-        }
+function mouseOver(place) {
+    let quote; 
+    document.querySelector('#quote-container').addEventListener('mouseover', function mouseQuote(e) {
+        quote= quotes.find(quote=> quote.id === parseInt(e.target.id))
+        console.log(quote)
+        document.querySelector(`#Top${place}`).textContent =quote.quote
+        document.querySelector('#quote-container').removeEventListener('mouseover', mouseQuote)
     })
+    return quote
 }
+
 
 //Add a quote to the list
 document.querySelector('.quote-form').addEventListener('submit',handleQuote)
@@ -79,14 +107,13 @@ function handleQuote(e){
     let quoteObj = {
         quote: document.querySelector('#addQuote').value,
         author: document.querySelector('#addAuthor').value,
-        source: document.querySelector('#addSource').value
+        source: document.querySelector('#addSource').value,
+        like: 0
     }
     console.log(quoteObj)
     createQuote(quoteObj)
+
     //postNewQuote(quoteObj)
-    document.querySelector('#addQuote').value= ""
-    document.querySelector('#addAuthor').value = ""
-    document.querySelector('#addSource').value = ""
 }
 
 function postNewQuote(quoteObj){
